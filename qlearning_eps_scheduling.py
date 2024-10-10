f1c4e3b1eb7f5f97f24bcd585cee57eb0eccb319
THUAUD Simon <simounixx@gmail.com>
2024-10-10 10:02:19 +0200

    feat: added the report and change the calcul of qlearning epsilon

--- qlearning_eps_scheduling.py
+++ qlearning_eps_scheduling.py
@@ -43,14 +43,10 @@ class QLearningAgentEpsScheduling(QLearningAgent):
         action = self.legal_actions[0]
 
         # BEGIN SOLUTION
+        if self.timestep <= self.epsilon_decay_steps: # if we are still in the decay phase
+            self.epsilon = self.epsilon_start - (self.epsilon_start - self.epsilon_end) * self.timestep / self.epsilon_decay_steps
+            
         self.timestep += 1
-        self.epsilon = max(
-            self.epsilon_end,
-            self.epsilon_start
-            - (self.epsilon_start - self.epsilon_end)
-            * self.timestep
-            / self.epsilon_decay_steps,
-        )
         
         if random.uniform(0, 1) < self.epsilon:
             action = random.choice(self.legal_actions)
